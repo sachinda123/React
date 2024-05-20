7ecefe3c9dbea3ae19e3603169fafced4a9b846d
sachinda <sachinda@parkwaylabs.com>
2024-05-20 16:44:34 +0530

    on work

--- ui/src/reducers/signupReducer.js
+++ ui/src/reducers/signupReducer.js
@@ -8,21 +8,21 @@ const initialState = {
 
 const signupReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "SIGNIN_SUCCESS":
+    case SIGNIN_SUCCESS:
       return {
         ...state,
         isLoggedIn: true,
         user: action.payload,
         error: null,
       };
-    case "SIGNIN_FAILURE":
+    case SIGNIN_FAILURE:
       return {
         ...state,
         isLoggedIn: false,
         user: null,
         error: action.payload,
       };
-    case "SIGNIN_ERROR_RESET":
+    case SIGNIN_ERROR_RESET:
       return {
         ...state,
         isLoggedIn: false,
